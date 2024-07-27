13be64d58fd820dacf7a245373086fbfd5af3b63
ShobanaMuthiah <shobanam222@gmail.com>
2024-07-27 09:00:58 -1200

    completed

--- Controller/ViewersController.js
+++ Controller/ViewersController.js
@@ -5,11 +5,14 @@ dotenv.config();
 
 export const comments = async (req, res) => {
   const { name, email, description } = req.body;
+  console.log('Received form data:', { name, email, description });
   
   try {
     await sendEmail(email, `${name} - From Portfolio`, description);
+    console.log('Email sent successfully');
     res.status(200).json({ message: 'successful' });
   } catch (error) {
-    res.status(404).json({ message: 'Failed to send email', error });
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Failed to send email', error });
   }
 };
